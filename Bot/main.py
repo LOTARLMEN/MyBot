2896e55e90ba70c82aac9b846b5f85c3cdfc9b8e
VALERIY <val.janushkewitch@yandex.ru>
2024-10-30 13:30:26 +0200

    29.10

--- Bot/main.py
+++ Bot/main.py
@@ -12,13 +12,15 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    print('Привет! Я бот помогающий твоему здоровью!')
+    print('Нам написали /start!')
+    await message.answer('Привет!Я бот помогающий твоему здоровью!')
 
 
 
 @dp.message_handler()
 async def all_messages(message: types.Message):
-        print('Введите команду /start чтобы начать общение!')
+        print('Нам написали что-то кроме /start!')
+        await message.answer('Введите команду /start чтобы начать общение!')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
